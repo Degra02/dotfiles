1a0e2ea407e9c6744da6b065788014570b111814
Degra <filippodegrandi02@gmail.com>
2023-01-30 10:40:34 +0100

    refactored from Packer to Lazy

--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -17,6 +17,6 @@ require("nvim-tree").setup({
     group_empty = true,
   },
   filters = {
-    dotfiles = true,
+    dotfiles = false,
   },
 })
--- nvim/lua/degra/remap.lua
+++ nvim/lua/degra/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -7,7 +7,7 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- FloaTerm configuration
 
-vim.keymap.set("n", "<leader>ft", ":FloatermNew --name=term --height=0.8 --width=0.7 --autoclose=2 zsh <CR>")
+vim.keymap.set("n", "<leader>ft", ":FloatermNew --name=term --height=1.0 --width=0.8 --autoclose=2 zsh <CR>")
 vim.keymap.set("n", "t", ":FloatermToggle term<CR>")
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>:q<CR>")
 
