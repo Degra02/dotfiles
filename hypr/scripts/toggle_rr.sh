18f1bde74aa3cfb1e3573710d17a9c92b353ca6a
Degra02 <filippodegrandi02@gmail.com>
2024-03-27 15:05:02 +0100

    Changes

--- hypr/scripts/toggle_rr.sh
+++ hypr/scripts/toggle_rr.sh
@@ -4,19 +4,19 @@ get_framerate() {
   input_string=$(hyprctl monitors | grep -oP '\d+x\d+@\d+\.\d+')
 
   resolution_and_framerate=$(echo "$input_string" | grep -oP '\d+x\d+@\K\d+')
-  framerate=$(echo "$resolution_and_framerate" | cut -d'.' -f1)
+  framerate=$(echo "$resolution_and_framerate" | head -n1)
 
   echo $framerate
 }
 
-resolution_and_framerate=$(get_framerate)
+framerate=$(get_framerate)
 echo "Current framerate: $resolution_and_framerate"
 
-if [ $resolution_and_framerate -eq 165 ]; then
+if [ $framerate -eq 165 ]; then
   hyprctl keyword monitor eDP-1,2560x1600@60,0x0,1.25
-elif [ "$resolution_and_framerate" -eq 60 ]; then
+elif [ "$framerate" -eq 60 ]; then
   hyprctl keyword monitor eDP-1,2560x1600@165,0x0,1.25
 else
-    echo "No specific action defined for framerate $resolution_and_framerate"
+    echo "No specific action defined for framerate $framerate"
 fi
 
