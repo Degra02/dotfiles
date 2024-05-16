f0778c778494c60219f86d36a28f9dab071d9d0b
Degra02 <filippodegrandi02@gmail.com>
2024-05-16 13:09:22 +0200

    Changes

--- lvim/config.lua
+++ lvim/config.lua
@@ -12,24 +12,31 @@ lvim.plugins = {
   "AckslD/swenv.nvim",
   "stevearc/dressing.nvim",
 
+  -- hypr
+  {
+		"theRealCarneiro/hyprland-vim-syntax",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		ft = "hypr",
+	},
+
   -- Copilot
-  -- {
-  --   "zbirenbaum/copilot.lua",
-  --   cmd = "Copilot",
-  --   event = "InsertEnter",
-  --   config = function()
-  --     require("copilot").setup({})
-  --   end,
-  -- },
-  -- {
-  --   "zbirenbaum/copilot-cmp",
-  --   config = function()
-  --     require("copilot_cmp").setup({
-  --       suggestion = { enabled = false },
-  --       panel = { enabled = false }
-  --     })
-  --   end
-  -- },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({})
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    config = function()
+      require("copilot_cmp").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false }
+      })
+    end
+  },
 
   -- Colorschemes
   { "rose-pine/neovim",         name = "rose-pine" },
@@ -189,17 +196,17 @@ lvim.builtin.which_key.mappings["C"] = {
 
 -- Below config is required to prevent copilot overriding Tab with a suggestion
 -- when you're just trying to indent
--- local has_words_before = function()
---   if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
---   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
---   return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
--- end
--- local on_tab = vim.schedule_wrap(function(fallback)
---   local cmp = require("cmp")
---   if cmp.visible() and has_words_before() then
---     cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
---   else
---     fallback()
---   end
--- end)
--- lvim.builtin.cmp.mapping["<Tab>"] = on_tab
+local has_words_before = function()
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+end
+local on_tab = vim.schedule_wrap(function(fallback)
+  local cmp = require("cmp")
+  if cmp.visible() and has_words_before() then
+    cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+  else
+    fallback()
+  end
+end)
+lvim.builtin.cmp.mapping["<Tab>"] = on_tab
