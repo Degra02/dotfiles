30341f2e382c8eed9be61ba6d6cae207ce94c6aa
Degra02 <filippodegrandi02@gmail.com>
2024-07-19 18:02:45 +0200

    add zellij and other

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,9 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+-- temporary fix
+config.enable_wayland = false
+
 -- general
 config.enable_tab_bar = false
 
@@ -14,12 +17,16 @@ config.font_size = 16.0
 config.window_background_opacity = 0.90
 config.color_scheme = 'Oxocarbon Dark (Gogh)'
 
+config.window_padding = {
+  left = 2,
+  right = 2,
+  top = 0,
+  bottom = 0,
+}
+
 -- keybindings
 config.keys = {
   { key = 'Backspace', mods = 'CTRL', action = wezterm.action.SendKey { key = 'w', mods = 'CTRL' } },
 }
-
--- temporary fix
-config.enable_wayland = false
 
 return config
